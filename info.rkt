4bfc30bd205d5795f75005891930e4877bd5d86a
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-08-27 12:41:58 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,8 @@
 #lang info
 (define version "0.7")
 (define collection 'multi)
-(define pkg-desc "Procedures for continued fraction arithmetic and base conversion.")
+(define pkg-desc "Procedures for continued fraction arithmetic and base conversion.")(define deps '("base"
+               "math-lib"))
+(define build-deps '("racket-doc"
+                     "rackunit-lib"
+                     "scribble-lib"))
