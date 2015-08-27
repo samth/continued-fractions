a68afe9cd1bb898a4bb894da08b59d7e2e69a771
derend <deren.dohoda@gmail.com>
2015-08-27 12:28:39 -0400

    build-deps
    accidentally put build dependencies in regular dependencies. this is why
    we read documentation.

--- continued-fractions/info.rkt
+++ continued-fractions/info.rkt
@@ -1,3 +1,8 @@
 #lang info
 (define scribblings '(("cf-manual.scrbl" (multi-page))))
-(define deps (list "base" "math-lib" "racket-doc" "rackunit-lib" "scribble-lib"))
+(define deps (list "base"
+                   "math-lib"))
+(define build-deps (list "racket-doc"
+                         "rackunit-lib"
+                         "scribble-lib"))
+
